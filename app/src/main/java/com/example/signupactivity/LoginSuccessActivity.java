1b9fd19c071ec9fa1bac87a5e75cf06b64ae1f51
Deepak <srdeepak13@gmail.com>
2024-07-18 18:08:40 +0530

    c

---
+++ app/src/main/java/com/example/signupactivity/LoginSuccessActivity.java
@@ -0,0 +1,14 @@
+package com.example.signupactivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginSuccessActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_success);
+    }
+}
+
